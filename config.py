9f191ceb7e9e42761684d936a8a565a0fb96dc52
yourfriend143 <ramukakayourdada143@gmail.com>
2026-02-26 15:01:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ import os
 # Telegram Bot Credentials
 # ==============================
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", "0"))
-API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8579722162:AAHej40kAnYfJ2r_bzOZkWokZCUovpuMxeE")
+API_ID = int(os.environ.get("API_ID", "23283708"))
+API_HASH = os.environ.get("API_HASH", "7805011fb84729023531f0fa3f000bec")
 
 
 # ==============================
@@ -15,15 +15,15 @@ API_HASH = os.environ.get("API_HASH", "")
 # ==============================
 
 # Add admin user IDs separated by commas in environment variables
-ADMINS = [int(admin) for admin in os.environ.get("ADMINS", "").split(",") if admin]
+ADMINS = [int(admin) for admin in os.environ.get("ADMINS", "6481888008").split(",") if admin]
 
 
 # ==============================
 # Database Configuration
 # ==============================
 
-DB_URI = os.environ.get("DB_URI", "")
-DB_NAME = os.environ.get("DB_NAME", "SaveRestricted2")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://rj5706603:O95nvJYxapyDHfkw@cluster0.fzmckei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "rj5706603")
 
 
 # ==============================
@@ -31,7 +31,7 @@ DB_NAME = os.environ.get("DB_NAME", "SaveRestricted2")
 # ==============================
 
 # Replace with your Telegram log channel ID (example: -1001234567890)
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002363362847"))
 
 
 # ==============================
